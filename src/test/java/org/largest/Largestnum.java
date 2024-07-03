3aa0c2480a23ba5769d166a8a22ad9fe2173941b
Jenifer Augustin <jennyroshil@gmail.com>
2024-07-03 14:28:37 +0530

    raji pushed to github

---
+++ src/test/java/org/largest/Largestnum.java
@@ -0,0 +1,31 @@
+package org.largest;
+
+public class Largestnum {
+
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		int b=20;
+		int larg = a>b ? a:b ;
+	System.out.println(larg);
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
